bb910ccec6372da070e2cc91ad41d2592aba933e
BetterNameV2 <josemontero87@hotmail.com>
2014-12-27 17:33:41 +0100

    Planteamiento provisional de interfaces

---
+++ MapIntegrationApp/app/src/main/java/uo/sdm/mapintegrationapp/business/ArtifactCollection.java
@@ -0,0 +1,10 @@
+package uo.sdm.mapintegrationapp.business;
+
+/**
+ *
+ * Almacena y gestiona los artefactos del usuario.
+ *
+ * Created by Hans on 27/12/2014.
+ */
+public interface ArtifactCollection {
+}
---
+++ MapIntegrationApp/app/src/main/java/uo/sdm/mapintegrationapp/persistence/ZoneLoader.java
@@ -0,0 +1,10 @@
+package uo.sdm.mapintegrationapp.persistence;
+
+/**
+ *
+ * Gestiona la carga de zonas desde la base de datos.
+ *
+ * Created by Hans on 27/12/2014.
+ */
+public interface ZoneLoader {
+}
